6a684d7631b9c86a70fc04540f5f6b035adf5b09
Seongmo Gu <gusm96@naver.com>
2024-10-07 21:46:27 +0900

    Initial commit

---
+++ api-server/mbti-world/src/main/java/com/smsl/mbti_world/MbtiWorldApplication.java
@@ -0,0 +1,13 @@
+package com.smsl.mbti_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiWorldApplication.class, args);
+	}
+
+}
---
+++ api-server/mbti-world/src/test/java/com/smsl/mbti_world/MbtiWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smsl.mbti_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
